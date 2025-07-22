f1d258792b10c71625242fa8729dbc32bb5fca72
chromaticpipe <chromaticpipe@gmail.com>
2025-07-22 15:53:34 -0500

    Fix V_DrawFixedFill failing to cover the whole screen

--- src/hardware/hw_draw.c
+++ src/hardware/hw_draw.c
@@ -1450,8 +1450,8 @@ void HWR_DrawFixedFill(fixed_t x, fixed_t y, fixed_t w, fixed_t h, INT32 color)
 
 	if (!(color & V_NOSCALESTART))
 	{
-		/*
-		if (x == 0 && y == 0 && w == BASEVIDWIDTH && h == BASEVIDHEIGHT)
+		
+		if (x == 0 && y == 0 && w == (BASEVIDWIDTH<<FRACBITS) && h == (BASEVIDHEIGHT<<FRACBITS))
 		{
 			RGBA_t rgbaColour = palette[color&0xFF];
 			FRGBAFloat clearColour;
@@ -1462,7 +1462,7 @@ void HWR_DrawFixedFill(fixed_t x, fixed_t y, fixed_t w, fixed_t h, INT32 color)
 			HWD.pfnClearBuffer(true, false, &clearColour);
 			return;
 		}
-		*/
+		
 		
 		fx *= vid.dup;
 		fy *= vid.dup;
--- src/v_video.c
+++ src/v_video.c
@@ -1407,13 +1407,13 @@ void V_DrawFixedFill(fixed_t x, fixed_t y, fixed_t w, fixed_t h, INT32 c)
 	}
 	else
 	{
-		/*
-		if (x == 0 && y == 0 && w == BASEVIDWIDTH && h == BASEVIDHEIGHT)
+		
+		if (x == 0 && y == 0 && w == (BASEVIDWIDTH<<FRACBITS) && h == (BASEVIDHEIGHT<<FRACBITS))
 		{ // Clear the entire screen, from dest to deststop. Yes, this really works.
 			memset(screens[0], (c&255), vid.width * vid.height * vid.bpp);
 			return;
 		}
-		*/
+		
 		
 		x *= vid.dup;
 		y *= vid.dup;
